1e35a8cdb900195c8623a1e50575074a956f9fba
GitHub Action <action@github.com>
2026-08-14 09:00:31 +0000

    Update coverage badge

--- R/data.R
+++
@@ -1,7 +0,0 @@
-#' Cached information from the World Bank API
-#'
-#' This data is a cached result of the \code{\link{wb_cache}} function.
-#' By default functions \code{\link{wb_data}} and \code{\link{wb_search}} use this
-#' data for the \code{cache} parameter.
-#'
-"wb_cachelist"
--- data-raw/save-internal-data.R
+++
@@ -1,6 +0,0 @@
-source("data-raw/wb-api-name-patterns.R")
-source("data-raw/wb-api-parameters.R")
-
-usethis::use_data(wb_api_parameters, wb_api_name_patterns,
-  internal = TRUE, overwrite = TRUE
-)
